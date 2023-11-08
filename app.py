4b36e74e420fa98145540b8903cce058534e2024
vishnusathwik <vishnusathwik03@gmail.com>
2023-11-09 02:28:27 +0530

    added code

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def upload(filename):
 def transcribe(audio_url):
     transcript_request = {
         'audio_url': audio_url
+        #/transcript_id
     }
 
     transcript_response = requests.post(transcript_endpoint, json=transcript_request, headers=headers)
@@ -49,6 +50,7 @@ def poll(transcript_id):
     polling_endpoint = transcript_endpoint + '/' + transcript_id
     polling_response = requests.get(polling_endpoint, headers=headers)
     return polling_response.json()
+    
 def get_transcription_result_url(url):
     transcribe_id = transcribe(url)
     while True:
@@ -78,22 +80,20 @@ def s2t():
 @app.route('/chats2t', methods=['GET', 'POST'])
 def chats2t():
     if request.method == 'POST':
-        # Handle file upload
         global users
         users = extract_user_names('temp_whatsapp.txt')
 
         file = request.files['file']
         if file.filename != '':
-            # Save uploaded audio file temporarily
             file.save('temp_audio.mp3')
 
             # Upload and transcribe the audio file
             audio_url = upload('temp_audio.mp3')
             save_transcript(audio_url, 'transcript')
             parse('transcript.txt')
-            return redirect('/result')  # Redirect to the result page
-
+            return redirect('/result') 
     return render_template('chats2t.html')
+
 @app.route('/normals2t', methods=['GET', 'POST'])
 def normals2t():
     if request.method == 'POST':
@@ -187,7 +187,6 @@ def t2s():
         file = request.files['file']
 
         if file.filename != '':
-            # Save uploaded WhatsApp file temporarily
             file.save('temp_whatsapp.txt')
 
             # Format the uploaded file
@@ -226,11 +225,8 @@ def assign_voices():
             user_voice_map[user] = assigned_voice_id
         print(user_voice_map)
         generate_speech(chat_file,user_voice_map,'audio2')
-    
+
     return render_template('select_voices.html', users=users, voices=voices)
-
-
-
 def parse(filename):
     global users
     users = extract_user_names('temp_whatsapp.txt')
@@ -333,7 +329,7 @@ def combine_audio_files(output_folder, output_filename):
     audio_files.sort(key=lambda x: int(os.path.splitext(x)[0]))
 
     # Initialize an empty Wave_write object for the combined audio
-    combined_audio = wave.open(output_filename, 'wb')
+    combined_audio = wave.open(os.path.join("static", output_filename), 'wb')  # Save in templates folder
 
     # Initialize parameters based on the first audio file
     first_audio_file = os.path.join(output_folder, audio_files[0])
@@ -349,6 +345,55 @@ def combine_audio_files(output_folder, output_filename):
     # Close the combined audio file
     combined_audio.close()
 
+import pyaudio
+import wave
+
+def microphone():
+    FRAMES_PER_BUFFER = 3200
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 16000
+    p = pyaudio.PyAudio()
+
+    # starts recording
+    stream = p.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=RATE,
+        input=True,
+        frames_per_buffer=FRAMES_PER_BUFFER
+    )
+
+    print("Start recording...")
+
+    frames = []
+    seconds = 10
+    for i in range(0, int(RATE / FRAMES_PER_BUFFER * seconds)):
+        data = stream.read(FRAMES_PER_BUFFER)
+        frames.append(data)
+
+    print("Recording stopped")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open("output.wav", 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+@app.route('/record_audio', methods=['GET', 'POST'])
+def record_audio():
+    if request.method == 'POST':
+        # Call the microphone function to record audio
+        microphone()
+        audio_url = upload('output.wav')
+        save_transcript(audio_url, 'transcript')
+        return redirect('/result2')  # Redirect to the result page
+    return render_template('record_audio.html')  # Create an HTML template for the recording page
 
 
 if __name__ == '__main__':
